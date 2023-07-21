a2d1bee81e8ad00bfcff3da47e3e00c3462d590e
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-21 15:47:05 +0530

    dragstart, dragover and drop

---
+++ script.js
@@ -0,0 +1,26 @@
+let lists = document.getElementsByClassName('list')
+let rightBox = document.getElementById("right")
+let leftBox = document.getElementById('left')
+
+for (const list of lists) {
+    list.addEventListener('dragstart', (event) => {
+        let selected = event.target
+
+        rightBox.addEventListener('dragover', (event) => {
+            event.preventDefault()
+        })
+
+        rightBox.addEventListener('drop',(event)=>{
+            rightBox.appendChild(selected);
+            selected = null;
+        })
+        leftBox.addEventListener('dragover', (event) => {
+            event.preventDefault()
+        })
+
+        leftBox.addEventListener('drop',(event)=>{
+            leftBox.appendChild(selected);
+            selected = null;
+        })
+    })
+}
